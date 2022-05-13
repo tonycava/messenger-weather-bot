4ea7b16d005b3ae05cb80a308f82cb8704adc59b
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-13 21:06:28 +0200

    update return

--- src/index.js
+++ src/index.js
@@ -2,18 +2,62 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
+const request = require('request');
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: 'https://graph.facebook.com/v6.0/me/messages',
+      qs: {
+        access_token:
+          'EAAvOOplsX2IBAJMdwZAInTQSs0Hrcs1rtFRRn5HMevGEywWNyaPc86YBKky8aCRXpq7cqdU8S1AfqfWcIIPoo9vKZBwOPoHHM0cZAg7qJwdVyGsKQlEVVRlqpj5qafNFkL9Rh8xmpBFDCW93znrU67aMWvBQlr8ZCflSRCZAYAKmHitXPOql2',
+      },
+      method: 'POST',
+      json: request_body,
+    },
+    // eslint-disable-next-line no-unused-vars
+    (err, res, body) => {
+      if (!err) {
+        console.log('message sent!');
+      } else {
+        console.error('Unable to send message:' + err);
+      }
+    }
+  );
+}
+
 // Handles messages events
 // eslint-disable-next-line no-unused-vars
-function handleMessage(sender_psid, received_message) {}
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_post backs events
 // eslint-disable-next-line no-unused-vars
 function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-// eslint-disable-next-line no-unused-vars
-function callSendAPI(sender_psid, response) {}
-
+// eslint-disable-next-line no-unused-vars,no-redeclare
 app.post('/webhook', (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
@@ -29,6 +73,14 @@ app.post('/webhook', (req, res) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Return a '200 OK' response to all events

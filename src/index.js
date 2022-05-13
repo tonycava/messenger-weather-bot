27e0c40958441c28786737aa1f6fe280088e58fa
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-13 20:57:42 +0200

    psi id

--- src/index.js
+++ src/index.js
@@ -2,28 +2,45 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
+// Handles messages events
+// eslint-disable-next-line no-unused-vars
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_post backs events
+// eslint-disable-next-line no-unused-vars
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+// eslint-disable-next-line no-unused-vars
+function callSendAPI(sender_psid, response) {}
+
 app.post('/webhook', (req, res) => {
+  // Parse the request body from the POST
   let body = req.body;
-  // Checks this is an event from a page subscription
+
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 });
 
 app.get('/', (req, res) => {
-  res.send('jhhhhusi');
+  res.send('kkkk');
 });
 
 app.get('/webhook', (req, res) => {

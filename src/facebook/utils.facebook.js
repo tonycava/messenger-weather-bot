3755263f4bbd514d59b94641c4c52336582931d2
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-06-17 13:25:25 +0200

    capitalizr name but better

--- src/facebook/utils.facebook.js
+++ src/facebook/utils.facebook.js
@@ -120,5 +120,5 @@ module.exports = {
 };
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }

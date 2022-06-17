c5c7c377c4eabd41cf6bd1bba24cf409a4bf9ce2
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-06-17 09:57:04 +0200

    refractor7

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const routeFacebook = require('./facebook/route.facebook');
 const app = express();
 app.use(express.json());
 
-routeFacebook.get('/webhook');
+routeFacebook.router.get('/webhook');
+
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
